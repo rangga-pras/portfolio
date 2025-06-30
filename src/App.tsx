282dff6bc023eec9fddf70a3cef6e38cd3a3eaff
rangga-pras <muhrangga.prasetya@gmail.com>
2025-07-01 00:02:34 +0700

    chore: finalize App.tsx with complete section layout and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Navbar } from './components/Navbar';
+import { ScrollToTop } from "./components/ui/ScrollToTop";
 import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Skills } from './components/Skills';
+import Projects from './components/Projects';
+import { Blogs } from "./components/Blogs";
+import { Education } from './components/Education';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+
+
 
 function App() {
   return (
     <>
       <Navbar />
+      <ScrollToTop/>
       <Hero />
       <About />
       <Skills />
+      <Projects />
+      <Blogs />
+      <Education />
+      <Contact/>
+      <Footer />
     </>
   );
 }
